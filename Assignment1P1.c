47c1bd6527aeb8343565755227e753c7b8b35db1
Aryan Sahu <75617873+aryansahu400@users.noreply.github.com>
2021-02-15 13:37:35 +0530

    Add files via upload

---
+++ Assignment1P1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<limits.h>
+
+int main()
+{
+	printf("range of Short variable is from %d to %d\n", SHRT_MIN, SHRT_MAX);
+	printf("Range of Char variable is from %ld to %ld\n", CHAR_MIN, CHAR_MAX);
+	printf("Range of Integer variable is from %d to %d\n", INT_MIN, INT_MAX);
+	printf("Range of Long variable is from %ld to %ld\n", LONG_MIN, LONG_MAX);
+	printf("Range of Long Long variable is from %lld to %lld\n", LLONG_MIN, LLONG_MAX);
+	
+	
+
+	return 0;
+}
