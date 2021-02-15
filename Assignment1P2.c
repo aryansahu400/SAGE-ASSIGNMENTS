49fa42a6557d1a0c40d8b9e6eae33b16f5e6d4bf
Aryan Sahu <75617873+aryansahu400@users.noreply.github.com>
2021-02-15 14:42:30 +0530

    Add files via upload

---
+++ Assignment1P2.c
@@ -0,0 +1,17 @@
+//Program-2: Write a program to printing a variable of different datatypes.
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int i=8845;
+	long l=845843;
+	long long ll=43534534534;
+	char c=87;
+	float fl=7.5;
+	double d=84753475.45;
+	printf("integer = %d , long = %ld , long long = %lld , char = %c , float = %f , double d = %lf", i, l, ll, c, fl, d);
+
+	return 0;
+}
