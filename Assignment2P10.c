5c58277d0dfb69692400e6603162d7cd3053cdca
Aryan Sahu <75617873+aryansahu400@users.noreply.github.com>
2021-02-21 00:18:15 +0530

    Add files via upload

---
+++ Assignment2P10.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+struct customer
+{
+    int account_no;
+    char name[80];
+    int balance;
+};
+
+void accept(struct customer[], int);
+void display(struct customer[], int);
+int main()
+{
+    struct customer data[20];
+    int n, choice, account_no, amount, index;
+
+    printf("Banking System\n\n");
+    printf("Number of customer records you want to enter? : ");
+    scanf("%d", &n);
+    accept(data, n);
+    do
+    {
+
+        printf("\nBanking System Menu :\n");
+        printf("Press 1 to display all records.\n");
+        printf("Press 0 to exit\n");
+        printf("\nEnter choice(0-1) : ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+            case 1:
+                display(data, n);
+                break;
+        }
+    }
+    while (choice != 0);
+
+    return 0;
+}
+
+void accept(struct customer list[80], int s)
+{
+    int i;
+    for (i = 0; i < s; i++)
+    {
+        printf("\nEnter data for Record #%d", i + 1);
+
+        printf("\nEnter account_no : ");
+        scanf("%d", &list[i].account_no);
+        //fflush(stdin);
+        printf("Enter name : ");
+        scanf("%s",&list[i].name);
+        printf("Enter Blance: ");
+        scanf("%d",&list[i].balance);
+    } 
+}
+
+void display(struct customer list[80], int s)
+{
+    int i;
+
+    printf("\n\nA/c No\tName\tBalance\n");
+    for (i = 0; i < s; i++)
+    {
+        printf("%d\t%s\t%d\n", list[i].account_no, list[i].name,
+            list[i].balance);
+    } 
+}
